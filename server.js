226fcac03edee0bbe40265d642a6a006e5164a15
RitikRajput <ritik741rajput@gmail.com>
2026-07-19 23:24:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import "dotenv/config"
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res)=> {
+  res.json("hlo from server")
+})
+
+
+const PORT =process.env.PORT || 5000;
+
+
+app.listen(PORT, ()=>{
+  console.log(`SERVER is running at : ${PORT}`)
+})
